f705a7a0091a3729cb8309fbb948628dcad85645
Penvah <rpmanganyi@gmail.com>
2019-06-03 15:55:55 -0700

    first commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  array = [{name:"login",path:"login"},{name:"about",path:"about"},{name:"contact",path:"contact"}];
+
+  constructor(private router:Router) {}
+  
+
+
+  
+  ngOnInit() {
+  }
+onNavigate(item){
+  this.router.navigateByUrl(item.path)
+ 
+}
+
+}
+
+
+
